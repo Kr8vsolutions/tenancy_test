6dd07ef7e7f749b72d1506468a2a6a741da8f80f
Osa-96 <=>
2021-12-13 14:26:08 +0200

    tenanty page

--- routes/tenant.php
+++ routes/tenant.php
@@ -24,7 +24,7 @@ Route::middleware([
     PreventAccessFromCentralDomains::class,
 ])->group(function () {
     Route::get('/', function () {
-        return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
+        return view('auth.login');
     });
     Route::get('/users', function () {
         return response(['users'=>\App\Models\User::all()]);
